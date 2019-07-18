5f2c2b1ec13cce6a60f02c588b99a6f013f1434c
Vanessa da Paixao <vanessadapaixaobarros1@gmail.com>
2019-07-18 17:58:30 -0300

    Adicionar exemplo de JavaScript

---
+++ script.js
@@ -0,0 +1,13 @@
+let nome = "Vanessa"
+let anoDeNascimento = 2003
+
+
+console.log("Olá eu me chamo" + " " + nome + ", nasci no ano" + " " + anoDeNascimento + " " + "e estou estudando Javascript.")
+
+let nomeAluno = "Sophia"
+let matricula = 567429854_0
+let nota1 = 10
+let nota2 = 8
+let media = (nota1+nota2)/2;
+
+console.log("O aluno" + " " + nomeAluno + ", matricula" + " " + matricula + ", obteve a média final" + " " + media);
